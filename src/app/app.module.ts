13455be90943f7a18bd87bec45040efcef59363a
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-29 12:50:06 +0100

    user list and user details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
+import { HttpClientModule } from '@angular/common/http'; 
+
+/*
+Whenever you use the CLI to generate components and services, it will automatically 
+update this file to import and add them to the @NgModule decorator. Components are added 
+to the declarations array, and services are added as providers. You will also find yourself 
+adding various imports to the imports array. For instance, when we want to add animations, 
+we will add them here.
+*/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
