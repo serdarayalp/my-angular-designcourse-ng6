59c1210e471383c1979e63498d393ccea6f25d0f
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-29 13:20:39 +0100

    sidebar changes

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  
+  posts$: Object;
 
-  constructor() { }
+  constructor(private data: DataService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.data.getPosts().subscribe((data) => (this.posts$ = data));
   }
-
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, NavigationEnd } from '@angular/router';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  currentUrl: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    router.events.subscribe(
+      (_: NavigationEnd) => (this.currentUrl = this.router.url)
+    );
   }
 
+  ngOnInit(): void {}
 }
